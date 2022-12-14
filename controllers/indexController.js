3fb87e592b9969060489752b145469d66a881efe
Arianne Cardoso <comercial.arianne@gmail.com>
2022-12-14 12:12:23 -0300

    foi feita a função index  que exibe os produtos em ofertas na pagina home e a função search que faz a busca na barra do menu

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -18,9 +18,27 @@ const IndexController = {
     })
     console.log(products[0])
     res.render("index", { title: "Latech", products });
+  index: async (req, res) => {
+    let products = await Product.findAll({
+      raw: true,
+      include: [{
+          model: Image, as: 'images',
+      }],
+      where: {
+        idProdutos: idProdutosComDesconto
+      }
+  });    
+    products = products.map((product) => {
+      product.PrecoComDesconto = product.Preco * 0.7
+      return product
+    })
+    console.log(products[0])
+    res.render("index", { title: "Latech", products });
   },
 
+
   aboutUs: (req, res) => {
+
 
     res.render("aboutUs");
   },
@@ -46,8 +64,37 @@ const IndexController = {
         }        
         return product
       })
-    res.render("search", { title: "Latech", products });
-  }
+    res.render("indexx", { title: "Latech", products });
+  },
+
+  showProduct: (req, res) => {
+    let id = req.params.id;
+    // verificar nome da tabela produtos
+    let product = products.find((p) => p.id === id);
+    // verificar sequelize
+    res.render("produto.ejs", { product });
+  },
+  showCart: (req, res) => {
+    let userLoggedIn = req.session.user !== undefined;
+    if (!req.session.products) {
+      productsInTheCart = [];
+      res.render("cart.ejs");
+    } else {
+      let productsInTheCart = req.session.products.map((id) =>
+        products.find((p) => p.id == id)
+      );
+      res.render("cart", { productsInTheCart, userLoggedIn });
+    }
+  },
+  addCart: (req, res) => {
+    if (req.session.products) {
+      req.session.products.push(req.body.productChosen);
+    } else {
+      req.session.products = [req.body.productChosen];
+    }
+    res.redirect("/products");
+    console.log(req.session);
+  },
 
 };
 
